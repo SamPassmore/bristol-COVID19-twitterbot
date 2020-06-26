9cf65619d7c7c52a4b6c62f7b27fac4b7d1e4f74
Sam Passmore <passmore.sam@gmail.com>
2020-06-26 18:34:53 +0100

    changes to count days of no cases

--- bristol-covid19-bot.py
+++ bristol-covid19-bot.py
@@ -8,32 +8,7 @@ import re
 #dotenv.load_dotenv('.env')
 load_dotenv('.env')
 
-def main(code):
-
-	# Authenticate to Twitter
-	one = environ.get('ONE')
-	two = environ.get('TWO')
-	three = environ.get('THREE')
-	four = environ.get('FOUR')
-
-	auth = tweepy.OAuthHandler(one, two)
-	auth.set_access_token(three,four)
-
-	# # Create API object
-	api = tweepy.API(auth, wait_on_rate_limit=True,
-	    wait_on_rate_limit_notify=True)
-
-	# check authentication
-	try:
-	    api.verify_credentials()
-	    print("Authentication OK")
-	except:
-	    print("Error during authentication")
-
-	# # check if there is new bristol data
-	last_tweet = api.user_timeline(id = "@Bristol_C19", count = 1)[0].text
-
-
+def main(code, api):
 	d = pd.read_csv('covid-19-uk-data/data/covid-19-cases-uk.csv')
 	d = d[d.AreaCode==code]
   
@@ -63,37 +38,40 @@ def main(code):
 
 	# total cases
 	total = cases[-1]
+
+	# days of no cases
+	nocases = d[d.TotalCases == total]
+	date_nocases = datetime.strptime(nocases['Date'].iloc[0], "%Y-%m-%d")
+	days_nocases = (date - date_nocases).days
+
+	# previous tweets
+	lastten_tweets = api.user_timeline(id = "@Bristol_C19", count = 10)
+	# print(lastten_tweets[0].text)
+	for tweet in lastten_tweets:
+		prev_dates = re.search('There are [0-9]{3,} COVID-19 cases in Bristol as of ([A-Z]{1}[a-z]{2} [0-9]{2} [A-Z]{1}[a-z]{2})', tweet.text)
+		if(prev_dates is not None):			
+			if(prev_dates.group(1) == date_lwf):
+				week_count = re.search('There are ([0-9]{3,})', tweet.text).group(1) 
+			if(prev_dates.group(1) == date_yf):
+				last_tweet = tweet.text
   
 	# previous day
 	yesterday_count = re.search('There are ([0-9]{3,})', last_tweet).group(1)
 	day_inc = total - int(yesterday_count)
 	
-	# previous week
 
-	lastten_tweets = last_tweet = api.user_timeline(id = "@Bristol_C19", count = 10)
-	
-	print(date_lwf)
-
-	for tweet in lastten_tweets:
-		prev_dates = re.search('There are [0-9]{3,} COVID-19 cases in Bristol as of ([A-Z]{1}[a-z]{2} [0-9]{2} [A-Z]{1}[a-z]{2})', tweet.text)
-		
-		if(prev_dates is not None):			
-			if(prev_dates.group(1) == date_lwf):
-				week_count = re.search('There are ([0-9]{3,})', tweet.text).group(1) 
 
 	week_inc = total - int(week_count)
 
-	tweet = f"There are {total} COVID-19 cases in Bristol as of {date_f}. This is an increase of {day_inc} from {date_yf} and {week_inc} in the previous week."
-	#print(tweet)
+	if(days_nocases >= 3):
+		tweet = f"There have been {days_nocases} days since the last case of COVID-19 in Bristol. There has been {week_inc} cases in the previous week."
+	if(days_nocases < 3):
+		tweet = f"There are {total} COVID-19 cases in Bristol as of {date_f}. This is an increase of {day_inc} from {date_yf} and {week_inc} in the previous week."
+	# print(tweet)
 	# send tweet
 	return([tweet, last_tweet])
 
 
-# Parameters
-bristol_code = "E06000023"
-output = main(bristol_code)
-new_tweet = output[0]
-last_tweet = output[1]
 
 # API login
 # Authenticate to Twitter
@@ -116,8 +94,15 @@ try:
 except:
     print("Error during authentication")
 
+# Parameters
+bristol_code = "E06000023"
+output = main(bristol_code, api)
+new_tweet = output[0]
+last_tweet = output[1]
 
 if new_tweet != last_tweet:
+	print("#-- Old tweet --#")
+	print(last_tweet)
 	print("#-- New tweet --#")
 	print(new_tweet)
 	txt = input("Shall we tweet?\n")
